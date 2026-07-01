7ffc971cae9a8ee19bf44f8b113f3e0ca03b62f6
nmureire85 <nmureire@gmail.com>
2026-07-01 21:59:54 +0100

    High low number first draft implementation

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿namespace HighLowNumberGuessing;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the High Low Number Guesser!");
+
+
+        for (int guessCount = 0; guessCount < 10; guessCount++)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(1, 100);
+
+            Console.WriteLine("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            int difference = Math.Abs(number - randomNumber);
+
+            if (number < randomNumber)
+            {
+                Console.WriteLine("too low");
+            }
+
+            if (number > randomNumber)
+            {
+                Console.WriteLine("too high");
+            }
+
+            if (number == randomNumber)
+            {
+                Console.WriteLine("You won");
+                return;
+            }
+
+            if (difference <= 5)
+            {
+                Console.WriteLine($"You are close!. The random  number was {randomNumber}");
+            }
+
+            Console.WriteLine($"number of guesses : {guessCount} ");
+            if (guessCount < 5)
+            {
+                continue;
+            }
+
+            Console.WriteLine("You lose");
+            break;
+        }
+    }
+}
